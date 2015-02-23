0ccdb990037c9cc015049a88a24b39368735a954
Peter Nguyen <nguyenph88@gmail.com>
2015-02-23 17:13:56 -0400

    skeleton for the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntakeApp::Application.config.secret_token = '15aa13fb8e6d236bf86d571080e61742805eeefe2413e5745d125ee74bd8b81874d92a8a8b2b6bc79e5bdf06516ca6fbe9e618e42a624d058a5b35fc6f4364a2'
