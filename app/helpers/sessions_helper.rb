03ea6e4adc9224982f030a0d2ff9b2fbb98cdf40
Peter Nguyen <nguyenph88@gmail.com>
2015-03-09 16:23:45 -0300

    finish login, user controller, distinguish bw Partner and Admin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,11 +12,12 @@ module SessionsHelper
   
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.where(id: session[:user_id]).first
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 IntakeApp::Application.routes.draw do
+  get "users/new"
+
   get "sessions/new"
 
   #root                'static_pages#home
-  #get    'signup'  => 'users#new'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
