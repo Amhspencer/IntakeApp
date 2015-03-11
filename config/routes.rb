8c4f380ee932051d001815b812d12c9118433f7e
Ali Kayes <alikayes@berkeley.edu>
2015-03-10 18:36:37 -0700

    got admin view working

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ IntakeApp::Application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
 
+  get 'admin/index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
