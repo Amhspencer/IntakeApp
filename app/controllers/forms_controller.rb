13ebca81b73f20cf1f9bf013466addd69500d8d5
Michael <mlebow@gmail.com>
2015-03-10 20:26:39 -0700

    Updated the forms controller

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,6 +1,21 @@
 class FormController < ApplicationController
 
   def show
-
+    id = params[:id]
+    @form = Form.find(id)
+    # Should render the default show route
   end
+
+
+
+  def new
+    
+  end
+
+  def create
+    @form = Form.create!(params[:form])
+    # Redirect to the end page, for now redirects to a show that form
+    redirect_to form_path(@form)
+  end
+
 end
