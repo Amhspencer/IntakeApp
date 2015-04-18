62032ddd7689fdc72eaaf4dd1e794466c6af903d
Dante Natoli <dnatoli@berkeley.edu>
2015-04-17 23:11:17 -0300

    Maybe submitting

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe AdminsController do
 
   describe 'showing the admin dashboard page' do
-    it 'should call the validation method to ensure I am an admin' do
+
+    it 'should call the validation method to ensure I am an admin'
       
-    end
-    it 'should call the method for sorting forms' do
-    end
-    it 'should select the admin dashboard for rendering' do
-    end
-    it 'should make the sorted forms available to the dashboard' do
-    end
+    it 'should call the method for sorting forms'
+
+    it 'should select the admin dashboard for rendering'
+    
+    it 'should make the sorted forms available to the dashboard'
+
   end
 
 end
